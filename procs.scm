28cb328be7eb80958ec56472bdded50b195a4fbd
Dmitriy Nemykin <ddraudred@gmail.com>
2020-02-18 13:08:52 +0300

    Conditional + procs

---
+++ procs.scm
@@ -0,0 +1,21 @@
+(define (puts v)
+  (display v)
+  (newline)
+)
+
+(define (square x) (* x x))
+
+(puts (square 21))
+(puts (square (square 3)))
+
+(define (sum_of_squares x y)
+  (+ (square x) (square y))
+)
+
+(puts (sum_of_squares 3 4))
+
+(define (f a)
+  (sum_of_squares (+ a 1) (* a 2))
+)
+
+(puts (f 5))
