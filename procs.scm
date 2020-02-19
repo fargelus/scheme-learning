1928baeb1ae20395552ba2d1f674720feb7a80b3
Dmitriy Nemykin <ddraudred@gmail.com>
2020-02-19 11:41:49 +0300

    Sum of squares between three nums

--- procs.scm
+++ procs.scm
@@ -25,3 +25,24 @@
 )
 
 (puts (>= 3 4))
+
+(define (max x y)
+  (if (> x y) x y)
+)
+(define (min x y)
+  (if (< x y) x y)
+)
+(display "Max(4, 5) = ")
+(puts (max 4 5))
+
+(define (largest_sum_of_squares x y z)
+  (define max_num (max (max x y) z))
+  (define second_max
+    (cond ((= (- max_num x) 0) (max y z))
+          ((= (- max_num y) 0) (max x z))
+          ((= (- max_num z) 0) (max x y))
+    )
+  )
+  (sum_of_squares max_num second_max)
+)
+(puts (largest_sum_of_squares 1 3 6))
