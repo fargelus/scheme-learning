b8c40165f1dc0644a5c76bb56943b54e1ec6f485
Dmitriy Nemykin <ddraudred@gmail.com>
2020-03-07 15:04:34 +0300

    Pascal triangle row

--- pascal_triangle.scm
+++ pascal_triangle.scm
@@ -1,25 +1,34 @@
+(define (puts v)
+  (display v)
+  (newline))
+
 (define (pascal-triangle row)
-  (define (calc-el row, pos)
+  (define (calc-el row pos)
     (if (one? row pos)
           1
           (+ (calc-el (- row 1) (- pos 1))
              (calc-el (- row 1) pos))))
 
   (define (one? row pos)
-    (or (= pos 0) (< pos (- row 1))))
+    (or (= pos 0) (= pos (- row 1))))
 
   (define (print el last?)
     (define str-el (number->string el))
     (if (not last?)
         (display (string-append str-el " "))
-        (display str-el)))
+        (puts str-el)))
 
+  (define (print-element-from-pos i)
+    (define el (calc-el row i))
+    (if (= (+ i 1) row)
+        (print el #t)
+        (print el #f)))
 
-  (define i 0)
   (define (calc-el-iter i)
     (if (< i row)
-        (print ((calc-el row i) #f)
-        ;TODO доделать
+        ((print-element-from-pos i)
+         (calc-el-iter (+ i 1)))))
 
+  (calc-el-iter 0))
 
-(pascal-triangle 2)
+(pascal-triangle 5)
