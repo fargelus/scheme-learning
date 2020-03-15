afbc191f89ff6bd44d2a8b6c6e26f0aa5815edcd
Dmitriy Nemykin <ddraudred@gmail.com>
2020-03-15 14:50:42 +0300

    Fast exponential

---
+++ fast_expt.scm
@@ -0,0 +1,12 @@
+(define (puts v)
+  (display v)
+  (newline))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (square x) (* x x))
+
+(puts (fast-expt 2 3))
